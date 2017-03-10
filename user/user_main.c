0f78c9c3cc88bb59d90e173c5a992d0421077feb
Sakari Kapanen <sakari.m.kapanen@gmail.com>
2017-03-10 13:44:53 +0200

    Update user_main

--- user/user_main.c
+++ user/user_main.c
@@ -33,28 +33,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define CONNECTION_TIMEOUT 10000
 #define DATA_FETCH_TIMEOUT 10000
-#define QUEUE_LEN 1
 
 os_timer_t timeout_timer;
-os_event_t task_queue[QUEUE_LEN];
 
 u8g2_t u8g2;
-
-void compute(os_event_t *e) {
-    static uint32_t time = 0;
-    system_os_post(2, 0, 0);
-
-    uint32_t new_time = system_get_time();
-    os_printf("%u\n", new_time - time);
-    time = new_time;
-
-    uint32_t x = (new_time * 128 / 2000000) % 128;
-
-    u8g2_ClearBuffer(&u8g2);
-    u8g2_SetFont(&u8g2, u8g2_font_profont12_tf);
-    u8g2_DrawStr(&u8g2, x, 20, "Hello World!");
-    u8g2_SendBuffer(&u8g2);
-}
 
 void oled_init(void) {
     u8g2_Setup_ssd1306_128x64_noname_f(&u8g2, U8G2_R2,
@@ -62,14 +44,24 @@ void oled_init(void) {
         u8x8_gpio_and_delay_esp8266);  // init u8g2 structure
     u8g2_InitDisplay(&u8g2); // send init sequence to the display, display is in sleep mode after this,
     u8g2_SetPowerSave(&u8g2, 0); // wake up display
+
+    u8g2_ClearBuffer(&u8g2);
+    u8g2_SetFontPosTop(&u8g2);
+    u8g2_SetFont(&u8g2, u8g2_font_profont12_tf);
+    u8g2_DrawStr(&u8g2, 0, 0, "Hello World!");
+    u8g2_SendBuffer(&u8g2);
 }
 
 void http_get_callback(char * response_body, int http_status,
     char * response_headers, int body_size) {
-    if (http_status == 200) {
-        os_printf("Got 200 OK, response:\n%s\n", response_body);
-    } else {
-        os_printf("Got code 200 %d\n", http_status);
+    os_printf("HTTP status %d\n", http_status);
+
+    if (response_headers != NULL) {
+        os_printf("Headers:\n%s\n", response_headers);
+    }
+
+    if (response_body != NULL) {
+        os_printf("Body:\n%s\n", response_body);
     }
 }
 
@@ -85,7 +77,7 @@ void wifi_connect_cb(bool connected) {
         os_timer_setfn(&timeout_timer, (os_timer_func_t *)go_to_sleep, NULL);
         os_timer_arm(&timeout_timer, DATA_FETCH_TIMEOUT, false);
 
-        http_get("http://example.com/", "", http_get_callback);
+        http_get_streaming("http://example.com", "", http_get_callback);  // Example domain for testing for now - this sends chunked responses
     } else {
         os_printf("Connection failed\n");
         go_to_sleep();
@@ -96,8 +88,8 @@ void user_init(void) {
     system_update_cpu_freq(80);
     uart_init(BIT_RATE_115200, BIT_RATE_115200);
 
-    wifi_station_init(WIFI_SSID, WIFI_PWD, wifi_connect_cb, CONNECTION_TIMEOUT);
-
     oled_init();
+
+    wifi_station_init(WIFI_SSID, WIFI_PWD, wifi_connect_cb, CONNECTION_TIMEOUT);
 }
 
