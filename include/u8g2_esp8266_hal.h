860cff9af88550632fb6321e4bf12d814a44b66e
Sakari Kapanen <sakari.m.kapanen@gmail.com>
2017-03-22 00:16:46 +0200

    brzo_i2c and I2C HAL for U8G2

--- include/u8g2_esp8266_hal.h
+++ include/u8g2_esp8266_hal.h
@@ -6,23 +6,47 @@
 
 #include "u8g2.h"
 
-#define SSD1306_D0_MUX PERIPHS_IO_MUX_MTMS_U
-#define SSD1306_D0_FUNC FUNC_GPIO14
-#define SSD1306_D0_GPIO 14
-#define SSD1306_D1_MUX PERIPHS_IO_MUX_MTCK_U
-#define SSD1306_D1_FUNC FUNC_GPIO13
-#define SSD1306_D1_GPIO 13
-#define SSD1306_RESET_MUX PERIPHS_IO_MUX_GPIO4_U
-#define SSD1306_RESET_FUNC FUNC_GPIO4
-#define SSD1306_RESET_GPIO 4
-#define SSD1306_DC_MUX PERIPHS_IO_MUX_GPIO5_U
-#define SSD1306_DC_FUNC FUNC_GPIO5
-#define SSD1306_DC_GPIO 5
-#define SSD1306_CS_MUX PERIPHS_IO_MUX_GPIO0_U
-#define SSD1306_CS_FUNC FUNC_GPIO0
-#define SSD1306_CS_GPIO 0
+// Overclock the I2C bus
+#define U8G2_ESP8266_HAL_I2C_FAST_PLUS
+
+// Use SPI?
+//#define U8G2_ESP8266_4WIRE_SPI
+
+
+#ifdef U8G2_ESP8266_4WIRE_SPI
+// This pin definition (SCK) applies only for the software SPI but the default
+// is the same as the HW SCK pin.
+#define U8G2_ESP8266_D0_MUX PERIPHS_IO_MUX_MTMS_U
+#define U8G2_ESP8266_D0_FUNC FUNC_GPIO14
+#define U8G2_ESP8266_D0_GPIO 14
+// MOSI pin used by SW SPI. By default the same as the HW MOSI pin.
+#define U8G2_ESP8266_D1_MUX PERIPHS_IO_MUX_MTCK_U
+#define U8G2_ESP8266_D1_FUNC FUNC_GPIO13
+#define U8G2_ESP8266_D1_GPIO 13
+#define U8G2_ESP8266_RESET_MUX PERIPHS_IO_MUX_GPIO4_U
+#define U8G2_ESP8266_RESET_FUNC FUNC_GPIO4
+#define U8G2_ESP8266_RESET_GPIO 4
+#define U8G2_ESP8266_DC_MUX PERIPHS_IO_MUX_GPIO5_U
+#define U8G2_ESP8266_DC_FUNC FUNC_GPIO5
+#define U8G2_ESP8266_DC_GPIO 5
+#define U8G2_ESP8266_CS_MUX PERIPHS_IO_MUX_GPIO0_U
+#define U8G2_ESP8266_CS_FUNC FUNC_GPIO0
+#define U8G2_ESP8266_CS_GPIO 0
+#else
+// I2C pins: D0 == SCL, D1 == SDA. The implementation is software, use any pins
+// you desire.
+#define U8G2_ESP8266_D0_MUX PERIPHS_IO_MUX_GPIO5_U
+#define U8G2_ESP8266_D0_FUNC FUNC_GPIO5
+#define U8G2_ESP8266_D0_GPIO 5
+#define U8G2_ESP8266_D1_MUX PERIPHS_IO_MUX_GPIO4_U
+#define U8G2_ESP8266_D1_FUNC FUNC_GPIO4
+#define U8G2_ESP8266_D1_GPIO 4
+#endif
 
 uint8_t u8x8_gpio_and_delay_esp8266(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr);
 
 uint8_t u8x8_byte_esp8266_hw_spi(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr);
+// Brzo_i2c by pasko-zh for a faster I2C implementation:
+// https://github.com/pasko-zh/brzo_i2c
+uint8_t u8x8_byte_brzo_sw_i2c(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr);
 
--- lib/u8g2_esp8266_hal.c
+++ lib/u8g2_esp8266_hal.c
@@ -5,14 +5,14 @@
 #include <driver/spi.h>
 #include <driver/spi_interface.h>
 #include <espmissingincludes.h>
+#include <gpio.h>
 
-void set_gpio(uint8_t gpio_num, uint8_t value) {
-    if (value) {
-        GPIO_REG_WRITE(GPIO_OUT_W1TS_ADDRESS, 1 << gpio_num);
-    } else {
-        GPIO_REG_WRITE(GPIO_OUT_W1TC_ADDRESS, 1 << gpio_num);
-    }
-}
+#include "brzo_i2c.h"
+
+#define set_gpio(gpio_num, value) GPIO_REG_WRITE(((value) ? \
+    GPIO_OUT_W1TS_ADDRESS : GPIO_OUT_W1TC_ADDRESS), (1 << (gpio_num)))
+#define enable_gpio(gpio_num, enable) GPIO_REG_WRITE(((enable) ? \
+    GPIO_ENABLE_W1TS_ADDRESS : GPIO_ENABLE_W1TC_ADDRESS), (1 << (gpio_num)))
 
 uint8_t u8x8_gpio_and_delay_esp8266(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr)
 {
@@ -21,24 +21,26 @@ uint8_t u8x8_gpio_and_delay_esp8266(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, 
         case U8X8_MSG_GPIO_AND_DELAY_INIT:  // called once during init phase of u8g2/u8x8
             ETS_GPIO_INTR_DISABLE();
 
-            PIN_FUNC_SELECT(SSD1306_D0_MUX, SSD1306_D0_FUNC);
-            PIN_FUNC_SELECT(SSD1306_D1_MUX, SSD1306_D1_FUNC);
+            PIN_FUNC_SELECT(U8G2_ESP8266_D0_MUX, U8G2_ESP8266_D0_FUNC);
+            PIN_FUNC_SELECT(U8G2_ESP8266_D1_MUX, U8G2_ESP8266_D1_FUNC);
 
-            PIN_FUNC_SELECT(SSD1306_RESET_MUX, SSD1306_RESET_FUNC);
-            PIN_FUNC_SELECT(SSD1306_CS_MUX, SSD1306_CS_FUNC);
-            PIN_FUNC_SELECT(SSD1306_DC_MUX, SSD1306_DC_FUNC);
+            uint32_t gpio_mask = (1 << U8G2_ESP8266_D0_GPIO) | (1 << U8G2_ESP8266_D1_GPIO);
 
-            uint32_t gpio_mask =
-                (1 << SSD1306_D0_GPIO) | (1 << SSD1306_D1_GPIO) |
-                (1 << SSD1306_RESET_GPIO) | (1 << SSD1306_DC_GPIO) |
-                (1 << SSD1306_CS_GPIO);
-            GPIO_REG_WRITE(GPIO_ENABLE_W1TS_ADDRESS, gpio_mask);
+#ifdef U8G2_ESP8266_4WIRE_SPI
+            PIN_FUNC_SELECT(U8G2_ESP8266_RESET_MUX, U8G2_ESP8266_RESET_FUNC);
+            PIN_FUNC_SELECT(U8G2_ESP8266_CS_MUX, U8G2_ESP8266_CS_FUNC);
+            PIN_FUNC_SELECT(U8G2_ESP8266_DC_MUX, U8G2_ESP8266_DC_FUNC);
+            gpio_mask |=
+                (1 << U8G2_ESP8266_RESET_GPIO) | (1 << U8G2_ESP8266_DC_GPIO) |
+                (1 << U8G2_ESP8266_CS_GPIO);
+#endif
+            // Initially configure as output and low
             GPIO_REG_WRITE(GPIO_OUT_W1TC_ADDRESS, gpio_mask);
+            GPIO_REG_WRITE(GPIO_ENABLE_W1TS_ADDRESS, gpio_mask);
 
             ETS_GPIO_INTR_ENABLE();
             break;                            // can be used to setup pins
         case U8X8_MSG_DELAY_NANO:           // delay arg_int * 1 nano second
-            // An approximation which seems to suffice
             os_delay_us(arg_int == 0 ? 0 : 1);
             break;
         case U8X8_MSG_DELAY_100NANO:        // delay arg_int * 100 nano seconds
@@ -50,21 +52,34 @@ uint8_t u8x8_gpio_and_delay_esp8266(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, 
         case U8X8_MSG_DELAY_MILLI:          // delay arg_int * 1 milli second
             os_delay_us(1000 * arg_int);
             break;
+        case U8X8_MSG_DELAY_I2C:
+#ifndef U8G2_ESP8266_HAL_I2C_FAST_PLUS
+            os_delay_us(arg_int <= 2 ? 5 : 2);
+#endif
+            break;
+        case U8X8_MSG_GPIO_I2C_CLOCK:
+            enable_gpio(U8G2_ESP8266_D0_GPIO, arg_int == 0);
+            break;
+        case U8X8_MSG_GPIO_I2C_DATA:
+            enable_gpio(U8G2_ESP8266_D1_GPIO, arg_int == 0);
+            break;
+#ifdef U8G2_ESP8266_4WIRE_SPI
         case U8X8_MSG_GPIO_D0:              // D0 or SPI clock pin: Output level in arg_int
-            set_gpio(SSD1306_D0_GPIO, arg_int);
+            set_gpio(U8G2_ESP8266_D0_GPIO, arg_int);
             break;
         case U8X8_MSG_GPIO_D1:              // D1 or SPI data pin: Output level in arg_int
-            set_gpio(SSD1306_D1_GPIO, arg_int);
+            set_gpio(U8G2_ESP8266_D1_GPIO, arg_int);
             break;
         case U8X8_MSG_GPIO_CS:              // CS (chip select) pin: Output level in arg_int
-            set_gpio(SSD1306_CS_GPIO, arg_int);
+            set_gpio(U8G2_ESP8266_CS_GPIO, arg_int);
             break;
         case U8X8_MSG_GPIO_DC:              // DC (data/cmd, A0, register select) pin: Output level in arg_int
-            set_gpio(SSD1306_DC_GPIO, arg_int);
+            set_gpio(U8G2_ESP8266_DC_GPIO, arg_int);
             break;
         case U8X8_MSG_GPIO_RESET:           // Reset pin: Output level in arg_int
-            set_gpio(SSD1306_RESET_GPIO, arg_int);
+            set_gpio(U8G2_ESP8266_RESET_GPIO, arg_int);
             break;
+#endif
         default:
             u8x8_SetGPIOResult(u8x8, 1);          // default return value
             break;
@@ -129,6 +144,41 @@ uint8_t u8x8_byte_esp8266_hw_spi(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, voi
         case U8X8_MSG_BYTE_END_TRANSFER:
             u8x8->gpio_and_delay_cb(u8x8, U8X8_MSG_DELAY_NANO, u8x8->display_info->pre_chip_disable_wait_ns, NULL);
             u8x8_gpio_SetCS(u8x8, u8x8->display_info->chip_disable_level);
+            break;
+        default:
+            return 0;
+    }
+    return 1;
+}
+
+uint8_t u8x8_byte_brzo_sw_i2c(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr) {
+    uint8_t myerr;
+    static uint8_t msg_buf[256];
+    static size_t msg_len = 0;
+    switch(msg) {
+        case U8X8_MSG_BYTE_SEND:
+            if (msg_len + arg_int <= 256) {
+                os_memcpy(&msg_buf[msg_len], arg_ptr, arg_int);
+                msg_len += arg_int;
+            }
+            break;
+        case U8X8_MSG_BYTE_INIT:
+            brzo_i2c_setup(10);
+            break;
+        case U8X8_MSG_BYTE_SET_DC:
+            break;
+        case U8X8_MSG_BYTE_START_TRANSFER:
+            msg_len = 0;
+            break;
+        case U8X8_MSG_BYTE_END_TRANSFER:
+#ifdef U8G2_ESP8266_HAL_I2C_FAST_PLUS
+            brzo_i2c_start_transaction(u8x8_GetI2CAddress(u8x8) >> 1, 800);
+#else
+            brzo_i2c_start_transaction(u8x8_GetI2CAddress(u8x8) >> 1,
+                u8x8->display_info->i2c_bus_clock_100kHz * 100);
+#endif
+            brzo_i2c_write(msg_buf, msg_len, false);
+            myerr = brzo_i2c_end_transaction();
             break;
         default:
             return 0;
